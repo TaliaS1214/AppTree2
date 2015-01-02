7609745dddd6a0ab4e968f460467c207557de2e0
TaliaS1214 <talias432@gmail.com>
2015-01-02 18:05:02 -0500

    Moved views to Backbone

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  # Apps
+  resources :apps, only: [:index, :create, :show, :update]
+
+  # Genres
+  resources :apps
+  # Users
+
+  # Searches
+  resources :searches, only: [:create]
+
 end
