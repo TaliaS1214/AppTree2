e900804e21e83b4da0993c19018468940a75b903
TaliaS1214 <talias432@gmail.com>
2015-01-05 10:10:45 -0500

    Routes for getting json genres and apps

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,10 @@
 class AppsController < ApplicationController
 
+  def index
+    # Default Apps - Will Be Changed to Top This Week
+    render json: App.all.to_json, status: 200
+  end
+
   def create
   end
 
